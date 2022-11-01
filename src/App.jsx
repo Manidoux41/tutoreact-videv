620aa7e5a566be2f7ce09a919f0cb58976256a0e
superflyman90 <superflyman90@gmail.com>
2022-11-01 13:33:40 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/css/main.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+        <h1>Tuto React</h1>
+    </div>
+  )
+}
+
+export default App
