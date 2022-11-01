5ad824da546f18fb44b78428feff326bec268c7d
superflyman90 <superflyman90@gmail.com>
2022-11-01 14:12:18 +0100

    add key on li and new beharvior for delete

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,23 @@ import { useState } from "react"
 const App = () => {
 
 // state
-useState([
+const [fruits, setFruits] = useState([
     { id: 1, nom: "Abricot" },
     { id: 2, nom: "Banane" },
     { id: 3, nom: "Cerise" }
 ])
+const voiture = <li>Tesla</li>
 // comportements
-
+const handleDelete = () => {
+    console.log('handleDelete');
+}
 
 // affichage
   return (
     <div className='App'>
         <h1>Liste de fruits</h1>
-        <ul>
-            <li>Abricot</li>
-            <li>Banane</li>
-            <li>Cerise</li>
-        </ul>
+        <ul>{fruits.map((fruit) => 
+            <li key={fruit.id}>{fruit.nom} <button onClick={handleDelete}>X</button></li>)}</ul>
     </div>
   )
 }
