71cb8863c3b3ff1ddb8f99994755442e275e4446
superflyman90 <superflyman90@gmail.com>
2022-11-01 13:56:13 +0100

    cas simple de useState avec counter

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ const App = () => {
 const [counter, setCounter] = useState(1)
 
 // comportements
-
+const handleClick = () => {
+    setCounter(counter+ + 1)
+}
 
 // affichage
   return (
     <div className='App'>
         <h1>{counter}</h1>
+        <button onClick={handleClick}>Increment</button>
     </div>
   )
 }
